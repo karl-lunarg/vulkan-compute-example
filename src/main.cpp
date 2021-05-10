5cd77185bfc59ccf6f3888c25b6eaa52aa140c5c
Karl Schultz <karl@lunarg.com>
2021-05-10 06:59:58 -0600

    Example 4

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 
 auto main(int argc, char *argv[]) -> int
 {
-    const auto numElements = 6140;
+    const auto numElements = 100 * 1000 * 1000;
     const auto a = 2.0f; // saxpy scaling factor
 
     auto y = std::vector<float>(numElements, 0.71f);
@@ -18,5 +18,16 @@ auto main(int argc, char *argv[]) -> int
     auto out_tst = std::vector<float>{};
     d_y.to_host(out_tst); // and now out_tst should contain the result of saxpy (y = y + ax)
 
+    std::cout << "Starting result check..." << std::endl;
+    auto expectedResult = y[0] + a * x[0];
+    for (int i = 0; i < numElements; ++i)
+    {
+        if (out_tst[i] != expectedResult)
+        {
+            std::cout << "Result mismatch at index " << i << std::endl;
+            return 1;
+        }
+    }
+    std::cout << "Result OK" << std::endl;
     return 0;
 }
